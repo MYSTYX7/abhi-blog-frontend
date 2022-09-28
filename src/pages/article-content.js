7ef149dde229c600ddaaab331ccec2b6f668c8be
MYSTYX <cool.abhi@outlook.com>
2022-09-28 21:49:43 +0530

    Initial Commit

---
+++ src/pages/article-content.js
@@ -0,0 +1,25 @@
+const articles = [
+  {
+    name: "learn-react",
+    title: "The Fastest way to learn React",
+    thumbnail:'/images/blog1.jpg',
+    content: ["Hello Welcome to this article for learning react quick."],
+    date: "2020-01-01",
+  },
+  {
+    name: "learn-web",
+    title: "The Fastest way to learn web",
+    thumbnail:'/images/blog2.jpg',
+    content: ["Hello Welcome to this article for learning web quick."],
+    date: "2020-01-01",
+  },
+  {
+    name: "learn-js",
+    title: "The Fastest way to learn js",
+    thumbnail:'/images/blog3.jpg',
+    content: ["Hello Welcome to this article for learning js quick."],
+    date: "2020-01-01",
+  },
+];
+
+export default articles;
